b410af23f1fd8b88fc56a6cebaf9c5b5a2e1bedb
Miguel200132 <maicoteg@hotmail.com>
2024-06-13 16:47:47 -0600

    Cambio en el código de la tabla
    Se realizo un cambio en el código de la tabla para que la persona pudiera buscar el dato en especifico entre columnas

--- ProyectoDefinitiva/app.R
+++ ProyectoDefinitiva/app.R
@@ -155,14 +155,19 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$tabla_votos <- renderDT({
-  
-    datatable(votes, 
-              options = list(
-                searching = TRUE
-              )
-    )
+    datatable(votes, filter = 'top', options = list(
+      columnDefs = list(
+        list(targets = 1:4 - 1, searchable = TRUE) 
+      ),
+      format = list(
+        list(
+          type = 'num',
+          targets = c(0, 1, 2, 3)
+        )
+      )
+    ))
   })
 }
-
 shinyApp(ui = ui, server = server)
+
 
